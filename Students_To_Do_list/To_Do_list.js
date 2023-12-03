e88fd00f452f3839a1bd34fcaa043b677c34fb78
Zeid-Hussen <zeidhussen57@gmail.com>
2023-12-03 05:06:36 +0300

    cancel button and add task starts working

--- Students_To_Do_list/To_Do_list.js
+++ Students_To_Do_list/To_Do_list.js
@@ -2,3 +2,7 @@ document.querySelector(".add").addEventListener("click", function(){
   document.querySelector(".popup").classList.add("active");
 });
 
+document.querySelector(".but-cancel").addEventListener("click", function(){
+  document.querySelector(".popup").classList.remove("active");
+});
+
