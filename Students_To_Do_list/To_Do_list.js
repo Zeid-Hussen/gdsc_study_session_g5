33c4038c2c9a180956ed0116f35195f1c032582e
Zeid-Hussen <zeidhussen57@gmail.com>
2023-12-03 08:13:38 +0300

    revised... some features are disabled.

--- Students_To_Do_list/To_Do_list.js
+++ Students_To_Do_list/To_Do_list.js
@@ -9,13 +9,14 @@ document.querySelector(".but-cancel").addEventListener("click", function(){
 const ul = document.querySelector(".ul");
 const input1 = document.querySelector(".input-1");
 
+
 document.querySelector(".but-add").addEventListener("click", function(){
     if (input1.value === '') {
       alert("You must write something")
     }
     else {
       let list = document.createElement("li");
-      list.innerHTML = input1.value;
+      list.innerHTML = input1.value
       ul.appendChild(list) ;
     }
 })
