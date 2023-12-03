6082fa4c7f1d74224758c67b991cd5b8018af95f
Zeid-Hussen <zeidhussen57@gmail.com>
2023-12-03 13:20:36 +0300

    the listed layout handeled well

--- Students_To_Do_list/To_Do_list.js
+++ Students_To_Do_list/To_Do_list.js
@@ -8,15 +8,36 @@ document.querySelector(".but-cancel").addEventListener("click", function(){
 
 const ul = document.querySelector(".ul");
 const input1 = document.querySelector(".input-1");
-
+const input2 = document.querySelector(".input-2")
 
 document.querySelector(".but-add").addEventListener("click", function(){
-    if (input1.value === '') {
+    if (input1.value === '' || input2.value === '') {
       alert("You must write something")
     }
     else {
       let list = document.createElement("li");
-      list.innerHTML = input1.value
+      let close = document.createElement("div")
+      close.classList.add("close");
+      let closeIcon = document.createElement("span")
+      closeIcon.innerHTML = "\u2715";
+      closeIcon.classList.add("closeIcon");
+      let outerDiv = document.createElement("div");
+      outerDiv.classList.add("outerDiv");
+      let firstInput = document.createElement("div");
+      firstInput.textContent = input1.value;
+      firstInput.classList.add("firstInput");
+      let secondInput = document.createElement("div");
+      secondInput.textContent = input2.value;
+      secondInput.classList.add("secondInput");
+      close.appendChild(closeIcon);
+      outerDiv.appendChild(firstInput);
+      outerDiv.appendChild(secondInput);
+      list.appendChild(outerDiv);
+      list.appendChild(close);
       ul.appendChild(list) ;
+
     }
+  input1.value = "";
+  input2.value = "";
+  document.querySelector(".popup").classList.remove("active");
 })
